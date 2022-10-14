0fe4812d02e515c17439c9eef15f923c77dbba32
Christina Serveas P <christinaserveas@gmail.com>
2022-10-14 17:39:17 +0530

    Update stone_paper_sissor.py

--- stone_paper_sissor.py
+++ stone_paper_sissor.py
@@ -1,21 +1,24 @@
-#game
-from numpy import random
-my_click = ("rock", "paper", "sissor")
-random.choice(my_click)
-print(i)
-random_number = random.randint(1,10,1)[0]
-print(f"you have clicked: {random_number}")
-guess = 25
-while guess != random_number:
- if guess > 0:
-    if guess > random_number:
-        print(f"your choice {guess} is greater")
-    elif guess < random_number:
-        print(f"your choice {guess} is lesser")
+import random
+
+user_action = input("Enter a choice (rock, paper, scissors): ")
+possible_actions = ["rock", "paper", "scissors"]
+computer_action = random.choice(possible_actions)
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
     else:
-        print("sorry, better luck next time")
-        break
-    random_number = random.randint(1,10,1)[0]
-    print(f"random number is {random_number}")
-else:
-  print("you win")
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
