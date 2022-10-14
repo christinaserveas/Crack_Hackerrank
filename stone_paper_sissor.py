ad91c380c9af0ab4d29aa060d217d0af880ca2c5
Christina Serveas P <christinaserveas@gmail.com>
2022-10-14 16:49:45 +0530

    Update stone_paper_sissor.py

--- stone_paper_sissor.py
+++ stone_paper_sissor.py
@@ -1,21 +1,21 @@
 #game
 from numpy import random
-Clicks = ("rock", "paper", "sissor")
-random.choice(Clicks)
+my_click = ("rock", "paper", "sissor")
+random.choice(my_click)
 print(i)
 random_number = random.randint(1,10,1)[0]
-print(f"randomn number is {random_number}")
+print(f"you have clicked: {random_number}")
 guess = 25
 while guess != random_number:
  if guess > 0:
     if guess > random_number:
-        print(f"guess number {guess} is bigger")
+        print(f"your choice {guess} is greater")
     elif guess < random_number:
-        print( "guess numer {guess} is lesser")
+        print(f"your choice {guess} is lesser")
     else:
-        print("sorry that you're given up")
+        print("sorry, better luck next time")
         break
     random_number = random.randint(1,10,1)[0]
     print(f"random number is {random_number}")
 else:
-  print("congrats you win")
+  print("you win")
