b8c83d34a54b550ebc47acdf967a2ecfbbd84f92
Christina Serveas P <christinaserveas@gmail.com>
2022-10-14 19:45:30 +0530

    Create rankcard.py
    marklist

---
+++ rankcard.py
@@ -0,0 +1,8 @@
+import pandas as pd
+mark_sheet = pd.DataFrame({
+    'subject': ['physics','chemistry','biology','math'],
+    'Status': ['pass', 'pass', 'pass', 'pass'],
+    'mark': [7.5, 8.5, 8.9, 9.3],
+})
+
+print (mark_sheet)
